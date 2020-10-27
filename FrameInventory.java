41f6a3a11476849778d0d108ee0a50b9d2770d0f
jczl03 <66093859+jczl03@users.noreply.github.com>
2020-10-26 22:02:41 -0500

    Update FrameInventory.java

--- FrameInventory.java
+++ FrameInventory.java
@@ -1,130 +1,138 @@
-package Graphic;
+package Control;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import java.awt.Color;
-import javax.swing.GroupLayout;
-import javax.swing.GroupLayout.Alignment;
-import javax.swing.JLabel;
-import javax.swing.LayoutStyle.ComponentPlacement;
-import javax.swing.WindowConstants;
-import javax.swing.JTextField;
-import javax.swing.JButton;
-import javax.swing.ImageIcon;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-public class FrameInventory extends JFrame {
-
-	public JPanel contentPane;
-	public JTextField txtIdProducto;
-	public JTextField txtNombre;
-	public JTextField txtCantidad;
-	public JButton btnAnadir;
-	public JButton btnBuscar;
-	public JButton btnEliminar ;
-	private JLabel label_1;
-
-	/**
-	 * Create the frame.
-	 */
-	public FrameInventory() {
-		setForeground(Color.WHITE);
-		setTitle("Inventario");
-		setBackground(Color.DARK_GRAY);
-		initComponents();
-		createEvents();
+public class ConsultInventory extends Conexion{
+	
+	//methods that the form will call
+        public static void close(Connection conex){
+            try {
+		conex.close();
+            }catch(SQLException e) {
+		System.err.println(e);
+            }
+	
+        }
+		public boolean register(Inventory inventory) {
 		
+		PreparedStatement statement ;
+		Connection conex = getConexion();
+		
+		String sql = "INSERT  INTO inventario (nombre,cantidad,precio) VALUES(?,?,?)";
+			
+		try {
+			statement = conex.prepareStatement(sql);
+			statement.setString(1, inventory.getNombre());
+			statement.setInt(2, inventory.getCantidad());
+			statement.setInt(3, inventory.getPrecio());
+			statement.execute();
+			return true;
+		}catch(SQLException e) {
+			System.err.println(e);
+			return false;
+		}finally {
+                    close(conex);
+                }
 	}
-
-	private void createEvents() {
-		// TODO Auto-generated method stub
+	public boolean search(Inventory inventory) {
 		
+		PreparedStatement statement = null;
+		ResultSet res = null;
+		Connection conex = getConexion();
+		
+		String sql = "SELECT * FROM inventario WHERE nombre=?";
+		
+		try {
+			statement = conex.prepareStatement(sql);
+			statement.setString(1, inventory.getNombre());
+			res = statement.executeQuery();
+			
+			if(res.next()){
+				
+				inventory.setIdproducto(Integer.parseInt(res.getString("idproducto")));
+				inventory.setNombre(res.getString("nombre"));
+				inventory.setCantidad(res.getInt("cantidad"));
+                                inventory.setPrecio(res.getInt("precio"));
+				return true;
+			}
+			
+			return false;
+			
+		}catch(SQLException e) {
+			System.err.println(e);
+			return false;
+		}finally {
+                    close(conex);
+                }
 	}
-
-	private void initComponents() {
-		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-		setBounds(100, 100, 500, 225);
-		contentPane = new JPanel();
-		contentPane.setBackground(Color.DARK_GRAY);
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		JLabel lblNumeroDelProducto = new JLabel("Numero del producto:  ");
-		lblNumeroDelProducto.setForeground(Color.WHITE);
-		JLabel lNombre = new JLabel("Nombre: ");
-		lNombre.setForeground(Color.WHITE);
-		JLabel lblNewLabel = new JLabel("Cantidad: ");
-		lblNewLabel.setForeground(Color.WHITE);
-		txtIdProducto = new JTextField();
-		txtIdProducto.setColumns(10);
-		txtNombre = new JTextField();
-		txtNombre.setColumns(10);
-		txtCantidad = new JTextField();
-		txtCantidad.setColumns(10);
-		btnAnadir = new JButton("Añadir");
-		btnBuscar = new JButton("Buscar");
-		btnEliminar = new JButton("Eliminar");
+	public boolean delete(Inventory inventory) {
 		
-		label_1 = new JLabel("");
-		label_1.setIcon(new ImageIcon(FrameInventory.class.getResource("/Images/inventory.png")));
+		PreparedStatement statement ;
+		Connection conex = getConexion();
 		
-		GroupLayout gl_contentPane = new GroupLayout(contentPane);
-		gl_contentPane.setHorizontalGroup(
-			gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_contentPane.createSequentialGroup()
-					.addGap(18)
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addPreferredGap(ComponentPlacement.RELATED)
-							.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING, false)
-								.addGroup(gl_contentPane.createSequentialGroup()
-									.addComponent(lblNewLabel)
-									.addPreferredGap(ComponentPlacement.RELATED)
-									.addComponent(txtCantidad, GroupLayout.PREFERRED_SIZE, 219, GroupLayout.PREFERRED_SIZE))
-								.addGroup(gl_contentPane.createSequentialGroup()
-									.addComponent(lNombre)
-									.addPreferredGap(ComponentPlacement.RELATED)
-									.addComponent(txtNombre, GroupLayout.PREFERRED_SIZE, 228, GroupLayout.PREFERRED_SIZE))
-								.addGroup(Alignment.LEADING, gl_contentPane.createSequentialGroup()
-									.addComponent(lblNumeroDelProducto)
-									.addPreferredGap(ComponentPlacement.RELATED)
-									.addComponent(txtIdProducto)))
-							.addPreferredGap(ComponentPlacement.RELATED, 128, Short.MAX_VALUE)
-							.addComponent(label_1, GroupLayout.PREFERRED_SIZE, 120, GroupLayout.PREFERRED_SIZE))
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addGap(22)
-							.addComponent(btnAnadir, GroupLayout.PREFERRED_SIZE, 99, GroupLayout.PREFERRED_SIZE)
-							.addGap(37)
-							.addComponent(btnBuscar, GroupLayout.PREFERRED_SIZE, 99, GroupLayout.PREFERRED_SIZE)
-							.addGap(40)
-							.addComponent(btnEliminar, GroupLayout.PREFERRED_SIZE, 99, GroupLayout.PREFERRED_SIZE)))
-					.addContainerGap())
-		);
-		gl_contentPane.setVerticalGroup(
-			gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_contentPane.createSequentialGroup()
-					.addContainerGap()
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-						.addComponent(label_1, GroupLayout.PREFERRED_SIZE, 114, GroupLayout.PREFERRED_SIZE)
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-								.addComponent(lblNumeroDelProducto)
-								.addComponent(txtIdProducto, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-							.addGap(16)
-							.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
-								.addComponent(lNombre)
-								.addComponent(txtNombre, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-							.addGap(18)
-							.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-								.addComponent(lblNewLabel)
-								.addComponent(txtCantidad, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-							.addGap(35)
-							.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-								.addComponent(btnAnadir)
-								.addComponent(btnBuscar)
-								.addComponent(btnEliminar))))
-					.addContainerGap(18, Short.MAX_VALUE))
-		);
-		contentPane.setLayout(gl_contentPane);
+		String sql = "DELETE FROM inventario WHERE nombre=?";
 		
+		try {
+			statement = conex.prepareStatement(sql);
+			statement.setString(1, (inventory.getNombre()));
+			statement.execute();
+			return true;
+		}catch(SQLException e) {
+			System.err.println(e);
+			return false;
+		}finally {
+			close(conex);
+			}
 	}
-}	
+        public int getIdproducto(String nombre){
+            PreparedStatement statement;
+            ResultSet rs;
+	    Connection conex = getConexion();
+		
+            String sql = "SELECT * FROM inventario WHERE nombre=?";
+            try {
+                statement=conex.prepareStatement(sql);
+                statement.setString(1, nombre);
+                rs=statement.executeQuery();
+                if(rs.next()){
+                    return rs.getInt("idproducto");
+                }
+                
+                return -1;
+            } catch (SQLException ex) {
+                Logger.getLogger(ConsultInventory.class.getName()).log(Level.SEVERE, null, ex);
+                return -1;
+            }finally{
+                close(conex);
+            }
+        }
+        public float getCosto(int id){
+            PreparedStatement statement;
+            ResultSet rs;
+	    Connection conex = getConexion();
+		
+            String sql = "SELECT * FROM inventario WHERE idproducto=?";
+            try {
+                statement=conex.prepareStatement(sql);
+                statement.setInt(1, id);
+                rs=statement.executeQuery();
+                if(rs.next()){
+                    return rs.getFloat("precio");
+                }
+                
+                return -1;
+            } catch (SQLException ex) {
+                Logger.getLogger(ConsultInventory.class.getName()).log(Level.SEVERE, null, ex);
+                return -1;
+            }finally{
+                close(conex);
+            }
+        }
+        
+	
+}
