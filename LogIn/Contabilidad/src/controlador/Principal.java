a193d9b2f828764e520ffda07d0eb9ca38ef0a8c
julianabanana <66394603+julianabanana@users.noreply.github.com>
2020-10-28 19:06:11 -0500

    Add files via upload

---
+++ LogIn/Contabilidad/src/controlador/Principal.java
@@ -0,0 +1,15 @@
+package controlador;
+
+import modelo.Logica;
+import vista.VentanaLogin;
+import vista.VentanaPrincipal;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Aplicacion miAplicacion = new Aplicacion();
+		miAplicacion.iniciarSistema();
+	}
+
+}
